05b956c605b20b4686c505c5fe971a9fc44ebb1f
bella wu <qwu025@uottawa.ca>
2025-06-04 13:20:00 -0400

    header

--- service/src/Header.js
+++ service/src/Header.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './Header.css';
 import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container';
 
 function Header() {
   return (
-  <Nav variant="tabs" defaultActiveKey="/home">
-        <Nav.Item>
-          <Nav.Link href="/home">Active</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">Option 2</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="disabled" disabled>
-            Disabled
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
+  <>
+      <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">
+            Art Website lol
+          </Navbar.Brand>
+          
+          <Nav defaultActiveKey="/home" as="ul">
+            <Nav.Item as="li">
+              <Nav.Link href="/home">
+                Home
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item as="li">
+              <Nav.Link eventKey="link-1">
+                Commission Info
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item as="li">
+              <Nav.Link eventKey="link-2">
+                Request Here
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item as="li">
+              <Nav.Link eventKey="link-2">
+                Gallery
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+          
+
+        </Container>
+      </Navbar>
+  </>
+        
   );
 }
 
