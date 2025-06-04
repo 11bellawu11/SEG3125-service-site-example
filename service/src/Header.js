0db19bd3a85488db89f7bac8e3f85f6af3eef96f
bella wu <qwu025@uottawa.ca>
2025-06-04 13:02:14 -0400

    imports and nav

---
+++ service/src/Header.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './Header.css';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Nav'
+
+function Header() {
+  return (
+  <Nav variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled" disabled>
+            Disabled
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+  );
+}
+
+export default Header;
