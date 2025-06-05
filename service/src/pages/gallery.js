00ea56af6d413222d3736b237590f1a7dad29160
bella wu <qwu025@uottawa.ca>
2025-06-05 09:24:52 -0400

    dupe galleries

--- service/src/pages/gallery.js
+++ service/src/pages/gallery.js
@@ -46,12 +46,63 @@ function Gallery() {
                   </Carousel.Item>
               </Carousel>
           </Col>
+        </Row>
+      </Container>
 
+      <Container className="container1">
+        <Row className="row1">
+          <Col className="col1">
+            <h5>
+              Headshot drawings!
+            </h5>
+          </Col>
+
+          <Col className="col1">
+              <Carousel activeIndex={index1} onSelect={handleSelect1}>
+                  <Carousel.Item>
+                      <img src={require('../images/20200307_190940000_iOS.jpg')} class="img-fluid" alt="..."></img>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                      <img src={require('../images/20200307_191006000_iOS.jpg')} class="img-fluid" alt="..."></img>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                      <img src={require('../images/20200307_191028000_iOS.jpg')} class="img-fluid" alt="..."></img>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                      <img src={require('../images/20200324_175234000_iOS.jpg')} class="img-fluid" alt="..."></img>
+                  </Carousel.Item>
+              </Carousel>
+          </Col>
+        </Row>
+      </Container>
+
+      <Container className="container1">
+        <Row className="row1">
+          <Col className="col1">
+            <h5>
+              Headshot drawings!
+            </h5>
+          </Col>
+
+          <Col className="col1">
+              <Carousel activeIndex={index1} onSelect={handleSelect1}>
+                  <Carousel.Item>
+                      <img src={require('../images/20200307_190940000_iOS.jpg')} class="img-fluid" alt="..."></img>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                      <img src={require('../images/20200307_191006000_iOS.jpg')} class="img-fluid" alt="..."></img>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                      <img src={require('../images/20200307_191028000_iOS.jpg')} class="img-fluid" alt="..."></img>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                      <img src={require('../images/20200324_175234000_iOS.jpg')} class="img-fluid" alt="..."></img>
+                  </Carousel.Item>
+              </Carousel>
+          </Col>
         </Row>
 
       </Container>
-
-
     </Container>
     
 
