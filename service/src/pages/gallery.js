ada40c2998b60556c06731ff1a9815cb67dc9ba8
bella wu <qwu025@uottawa.ca>
2025-06-04 22:41:10 -0400

    1 working gallery..

--- service/src/pages/gallery.js
+++ service/src/pages/gallery.js
@@ -1,27 +1,48 @@
 import Container from "react-bootstrap/esm/Container";
-import styles from './gallery.css';
-import HeadshotCarousel from "../components/headshot-carousel";
+import './gallery.css';
+
+import { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
 
 function Gallery() { 
+  const [index1, setIndex1] = useState(0);
+
+  const handleSelect1 = (selectedIndex) => {
+    setIndex1(selectedIndex);
+  };
+
 
   return(
-    
     <Container class="gallery-body">
-      <Container class="container1">
+      <Container className="container1">
         <h2>Welcome to the Gallery</h2>
         <h4>Here you will find some example works!</h4>
       </Container>
 
-      <Container class="container1">
-        
-        <Container class="container-images">
-        <HeadshotCarousel />
+      <Container className="container1">
+        <Container className="container-images">
+          <Carousel activeIndex={index1} onSelect={handleSelect1}>
+              <Carousel.Item>
+                  <img src={require('../images/20200307_190940000_iOS.jpg')} class="img-fluid" alt="..."></img>
+              </Carousel.Item>
+              <Carousel.Item>
+                  <img src={require('../images/20200307_191006000_iOS.jpg')} class="img-fluid" alt="..."></img>
+              </Carousel.Item>
+              <Carousel.Item>
+                  <img src={require('../images/20200307_191028000_iOS.jpg')} class="img-fluid" alt="..."></img>
+              </Carousel.Item>
+              <Carousel.Item>
+                  <img src={require('../images/20200324_175234000_iOS.jpg')} class="img-fluid" alt="..."></img>
+              </Carousel.Item>
+          </Carousel>
         </Container>
-        <Container class="container-images">
+
+        <Container className="container-images">
 
         </Container>
 
-        <Container class="container-images">
+        <Container className="container-images">
 
         </Container>
 
