1e23f357ddb20a8d478cfa28d1c27432e0c39ab6
bella wu <qwu025@uottawa.ca>
2025-06-05 19:38:53 -0400

    replace text

--- service/src/pages/home.js
+++ service/src/pages/home.js
@@ -30,6 +30,7 @@ function Home() {
         <div className="service-cards">
           <div className="service-card headshot"> 
             <img src={require('../images/Commission 75 - Zion-Showcase.png')} alt='headshot art of lady'></img>
+            <h2>Headshots</h2>
             <p>A focused portrait from the shoulders up, ideal for profile images, branding, or gifting. Simple, clean, and expressive </p>
             <Link to="/commission-info"  className='home-button request-work'>
               Learn More!
@@ -37,6 +38,7 @@ function Home() {
           </div>
           <div className="service-card midshot"> 
             <img src={require('../images/20240104_193355273_iOS.png')} alt='midshot image'></img>
+            <h2>Halfbody</h2>
             <p>A half-body illustration from the waist up. Perfect for showcasing clothing, posture, and expression. Suitable for character art, presentations, or promotional use.</p>
             <Link to="/commission-info"  className='home-button request-work'>
               Learn More!
@@ -44,6 +46,7 @@ function Home() {
           </div>
           <div className="service-card fullbody"> 
             <img src={require('../images/Commission 64 - Monoleaff_-Showcase.png')} alt='fullbody drawing'></img>
+            <h2>Fullbody</h2>
             <p>A complete illustration capturing the subject from head to toe. Recommended for dynamic poses, full outfits, and detailed character designs. Ideal for reference sheets, storytelling, or standout visuals.</p>
             <Link to="/commission-info"  className='home-button request-work'>
               Learn More!
