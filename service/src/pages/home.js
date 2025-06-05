5ce42953439d8baa49881c4c73739758ef891473
Enuma <136878147+Enuma04@users.noreply.github.com>
2025-06-05 19:15:12 -0400

    home page and request page change

--- service/src/pages/home.js
+++ service/src/pages/home.js
@@ -7,7 +7,8 @@ function Home() {
     <Container >
       <Container className="hero-home">
         <h1 className='home-header'>Welcome</h1>
-        <p className='home-message'>Art Website Lol is an Art Websit where we do Art Website things</p>
+        <p className='home-message'>At Art Website Lol, we turn your ideas into custom art,
+           whether it’s a headshot or a full body piece.</p>
        
           <Link to="/commission-request" className='home-button request-work'>
             Hire Us
@@ -29,6 +30,7 @@ function Home() {
         <div className="service-cards">
           <div className="service-card headshot"> 
             <img src='./headshot.PNG' alt='headshot art of lady'></img>
+            <h2>Headshots</h2>
             <p>A focused portrait from the shoulders up, ideal for profile images, branding, or gifting. Simple, clean, and expressive </p>
             <Link to="/commission-info"  className='home-button request-work'>
               Learn More!
@@ -36,6 +38,7 @@ function Home() {
           </div>
           <div className="service-card midshot"> 
             <img src='./midshot.jpg' alt='midshot image'></img>
+            <h2>Midshots</h2>
             <p>A half-body illustration from the waist up. Perfect for showcasing clothing, posture, and expression. Suitable for character art, presentations, or promotional use.</p>
             <Link to="/commission-info"  className='home-button request-work'>
               Learn More!
@@ -43,6 +46,7 @@ function Home() {
           </div>
           <div className="service-card fullbody"> 
             <img src='./fullbody.png' alt='fullbody drawing'></img>
+            <h2>Fullbody</h2>
             <p>A complete illustration capturing the subject from head to toe. Recommended for dynamic poses, full outfits, and detailed character designs. Ideal for reference sheets, storytelling, or standout visuals.</p>
             <Link to="/commission-info"  className='home-button request-work'>
               Learn More!
