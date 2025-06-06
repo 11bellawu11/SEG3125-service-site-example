7cebbd929b111f42f4c3c0fd01960006bdec4b11
bella wu <qwu025@uottawa.ca>
2025-06-05 21:30:53 -0400

    info page

--- service/src/pages/commission-info.js
+++ service/src/pages/commission-info.js
@@ -1,7 +1,15 @@
+import Container from "react-bootstrap/esm/Container";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './info.css';
+
 function CommissionInfo() {
   return (
-    
 
+    <Container className="info-body">
+      <h2 style={{ fontFamily: "'Caveat', cursive", fontSize: '2.5rem', color: "#D88C9A"}}>Welcome to the Gallery!</h2>
+      <h5 style={{color: "#D88C9A"}}>Here you will find some example works</h5>
+    </Container>
 
   );
 }
